4433fb9c76d27e376b9147135dc998eb960ffb5e
dcadenas-tradeit <dcadenas@tradeitsolutions.com>
2025-09-12 15:55:58 -0300

    New comp (#9)
    * style(MobileNavbar): add hide and show effect like tablet navbar
    
    * feat: redesign of booking component for home page
    
    * styles: improve gradient transition when hovering book button
    
    * chore: delete dead code
    
    * fix: mod setTimeOut to wait for the animation to finish when nav to Booking page
    
    ---------
    
    Co-authored-by: skyx20 <delmasprower@gmail.com>

--- src/components/SectionServices1.js
+++ src/components/SectionServices1.js
@@ -3,144 +3,15 @@ import emailjs from "@emailjs/browser";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import styles from "./SectionServices1.module.css";
+import CtaBooking from "./CtaBooking";
+import { useNavigate } from "react-router-dom";
 
 const SectionServices1 = ({ className = "" }) => {
-  const [formData, setFormData] = useState({
-    name: "",
-    phone: "",
-    email: "",
-  });
-
-  const [showMessageSentOverlay, setShowMessageSentOverlay] = useState(false);
-  const [statusMessage, setStatusMessage] = useState("");
-  const [isSuccess, setIsSuccess] = useState(null);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const serviceID = "service_8udmjzt";
-    const templateID = "template_a0fdrhk";
-    const publicKey = "xm_PgDq3DqQTHCiYS";
-
-    emailjs
-      .send(serviceID, templateID, formData, publicKey)
-      .then((response) => {
-        console.log("SUCCESS!", response.status, response.text);
-        setFormData({ name: "", phone: "", email: "" });
-
-        // Show full screen "message sent" overlay
-        setShowMessageSentOverlay(true);
-        setTimeout(() => {
-          setShowMessageSentOverlay(false);
-        }, 3000);
-      })
-      .catch((err) => {
-        console.log("FAILED...", err);
-        setStatusMessage("Failed to send message. Please try again later.");
-        setIsSuccess(false);
-
-        setTimeout(() => {
-          setStatusMessage("");
-          setIsSuccess(null);
-        }, 3000);
-      });
-  };
+  const navigate = useNavigate();
 
   return (
     <section className={[styles.sectionServices, className].join(" ")}>
-      <div className={styles.container}>
-        <div className={styles.grid}>
-          <div className={styles.receive}>
-            <div className={styles.grid1}>
-              <h1 className={styles.receiveAPersonal}>
-              Stay in touch with Kelly!
-              </h1>
-            </div>
-            <form onSubmit={handleSubmit}>
-              <div className={styles.receiveChild}>
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Name"
-                  value={formData.name}
-                  onChange={handleChange}
-                  required
-                  className={styles.inputField}
-                />
-              </div>
-              <div className={styles.contact}>
-                <input
-                  type="tel"
-                  name="phone"
-                  placeholder="Phone No"
-                  value={formData.phone}
-                  onChange={handleChange}
-                  required
-                  className={styles.inputField}
-                />
-              </div>
-              <div className={styles.receiveItem}>
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                  className={styles.inputField}
-                />
-              </div>
-
-              {statusMessage && (
-                <div
-                  className={
-                    isSuccess ? styles.successMessage : styles.errorMessage
-                  }
-                >
-                  {statusMessage}
-                </div>
-              )}
-
-              <button type="submit" className={styles.submit}>
-                <div className={styles.submitChild} />
-                <div className={styles.submit1}>Submit</div>
-              </button>
-            </form>
-          </div>
-
-          <div className={styles.receive1}>
-            <div className={styles.grid1} />
-            <h3 className={styles.bookKellyTo}>Book Kelly to Speak</h3>
-            <div className={styles.grid4}>
-              <Link to="/bookings" className={styles.gridChild}>
-                <div className={styles.bookLinkContent}>
-                  <h1 className={styles.bookHere}>
-                    Book here
-                    <img
-                      className={styles.pharrowUpRightLightIcon}
-                      loading="lazy"
-                      alt="Arrow Icon"
-                      src="/pharrowuprightlight.svg"
-                    />
-                  </h1>
-                </div>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* Full screen "Message Sent" overlay */}
-      {showMessageSentOverlay && (
-        <div className={styles.messageSentOverlay}>
-          <h1 className={styles.messageSentText}>Thank You! Message Sent</h1>
-        </div>
-      )}
+      <CtaBooking onBookingClick={() => navigate("/bookings")} />
     </section>
   );
 };
