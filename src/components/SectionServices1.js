17f99e8310cdeeb0bb06b0d5791e88e4370c541e
Sean Agbaje <seanaagbaje@your-email.com>
2025-09-26 01:36:53 +0300

    Fix unused imports for Netlify build

--- src/components/SectionServices1.js
+++ src/components/SectionServices1.js
@@ -1,6 +1,4 @@
-import React, { useState } from "react";
-import emailjs from "@emailjs/browser";
-import { Link } from "react-router-dom";
+
 import PropTypes from "prop-types";
 import styles from "./SectionServices1.module.css";
 import CtaBooking from "./CtaBooking";
